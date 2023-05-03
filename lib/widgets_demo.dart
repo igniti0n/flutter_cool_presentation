5d6781bfce04d297ec42e97281cb752bd4aadd2a
istajcer <ivan.stajcer@q.agency>
2023-05-03 23:55:14 +0200

    demo stuff

--- lib/widgets_demo.dart
+++ lib/widgets_demo.dart
@@ -83,7 +83,9 @@ class _AnimationsDemoState extends State<AnimationsDemo> {
   }
 }
 
-class WidgetsDemo extends StatefulWidget {
+//images/dash.png'
+//images/sunglasses.svg'
+class WidgetsDemo extends StatefulHookWidget {
   const WidgetsDemo({super.key});
 
   @override
@@ -95,40 +97,67 @@ class _WidgetsDemoState extends State<WidgetsDemo> {
 
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController();
+
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Image.asset('images/dash.png'),
-                SvgPicture.asset('images/sunglasses.svg')
-              ],
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            Text(
-              'I got $money in my bank account',
-              style: const TextStyle(
-                fontSize: 20,
-              ),
-            ),
+            MojSuperWidget(animationController: animationController),
+            const Text('Hey everyone!'),
+            Text('I have $money dollars in my bank account.!'),
+            if (money > 4) const Text('VECI SAM OD 4'),
+            if (money <= 4) const Text('MANJI SAM OD 4'),
             TextButton(
               onPressed: () {
-                setState(() => money++);
+                setState(() {
+                  money++;
+                  animationController.reset();
+                  animationController.forward();
+                });
               },
-              child: const Text(
-                'Add money',
-                style: TextStyle(fontSize: 16, color: Colors.green),
-              ),
-            ),
+              child: const Text('Add money'),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class MojSuperWidget extends StatelessWidget {
+  const MojSuperWidget({
+    super.key,
+    required this.animationController,
+  });
+
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset('images/dash.png')
+            .animate()
+            .moveX(delay: 0.ms, duration: 600.ms, begin: 200),
+        SvgPicture.asset('images/sunglasses.svg')
+            .animate(delay: 500.ms)
+            .fadeIn(duration: 400.ms)
+            .moveY(duration: 600.ms, begin: 700)
+            .animate(controller: animationController)
+            .shake(duration: 400.ms)
+      ],
+    );
+  }
+}
+ 
